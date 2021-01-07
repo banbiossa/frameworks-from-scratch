15ce77db6ceeb7ff6dc336d66d077d796a3b5968
Shota Shimizu <shota.shimizu@quantumblack.com>
2021-01-07 16:18:21 +0900

    update

--- frameworks_from_scratch/__init__.py
+++ frameworks_from_scratch/__init__.py
@@ -1,7 +1,12 @@
 # flake8: noqa
 __version__ = "0.1.0"
-from .function import Function, Square, Exp
-from .variable import Variable
-from .util import numerical_diff
+# from .function import Function, Square, Exp
+from .function import *
 
-__all__ = ["Function", "Square", "Exp", "Variable", "numerical_diff"]
+# from .variable import Variable
+from .variable import *
+
+# from .util import numerical_diff
+from .util import *
+
+# __all__ = ["Function", "Square", "Exp", "Variable", "numerical_diff"]
